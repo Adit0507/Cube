8ddc8315a6994ef1c387f46d830f562636998048
Adit0507 <adi05072002@gmail.com>
2025-07-16 18:02:16 +0530

    Task struct to model container execution and container needs

--- task/task.go
+++ task/task.go
@@ -1,12 +1,34 @@
 package task
 
-type State int 
+import (
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+)
+
+type State int
 
 const (
-	Pending State = iota
-	Scheduled 	//means system has figured out where to run the task
+	Pending   State = iota
+	Scheduled       //means system has figured out where to run the task
 	Running
 	Completed
 	Failed
 )
 
+type Task struct {
+	ID            uuid.UUID
+	ContainerID   string
+	Name          string
+	State         State
+	Image         string
+	CPU           float64
+	Memory        int               //will help the system identify no. of resources a task needs
+	Disk          int               //this too
+	ExposedPorts  nat.PortSet       //used by Docker to ensure machines allocates the proper network ports for the task
+	PortBindings  map[string]string //same as above
+	RestartPolicy string            //tells the system what to do in event a task stops or fails unexpectedly
+	StartTime     time.Time
+	FinishTime    time.Time
+}
