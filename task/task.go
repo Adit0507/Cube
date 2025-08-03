38aed34bdd8f5170440e8d06775d95fc9a8bb3a5
Adit0507 <adi05072002@gmail.com>
2025-08-03 17:56:36 +0530

    stopping tasks

--- task/task.go
+++ task/task.go
@@ -142,3 +142,26 @@ func (d *Docker) Run() DockerResult {
 
 	return DockerResult{ContainerID: resp.ID, Action: "start", Result: "success"}
 }
+
+func (d *Docker) Stop(id string) DockerResult {
+	log.Printf("Attempting to stop container %v", id)
+
+	ctx := context.Background()
+	err := d.Client.ContainerStop(ctx, id, container.StopOptions{})
+	if err != nil {
+		log.Printf("Error stopping container %s: %v\n", id, err)
+		return DockerResult{Error: err}
+	}
+
+	err = d.Client.ContainerRemove(ctx, id, container.RemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         false,
+	})
+	if err != nil {
+		log.Printf("error removing container %s: %v", id, err)
+		return DockerResult{Error: err}
+	}
+
+	return DockerResult{Action: "stop", Result: "success", Error: nil}
+}
