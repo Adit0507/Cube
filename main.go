4b3f77f6e77b79df12078619e0985878626eb4fd
Adit0507 <adi05072002@gmail.com>
2025-08-10 11:43:55 +0530

    creating and stopping container

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Adit0507/cube/node"
 	"github.com/Adit0507/cube/task"
 	"github.com/Adit0507/cube/worker"
+	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 )
@@ -33,11 +34,9 @@ func main() {
 	fmt.Printf("task event: %v\n", te)
 
 	w := worker.Worker{
-		Name:  "worker-1",
 		Queue: *queue.New(),
 		Db:    make(map[uuid.UUID]*task.Task),
 	}
-
 	fmt.Printf("worker: %v\n", w)
 	w.CollectionStats()
 	w.RunTask()
@@ -48,7 +47,7 @@ func main() {
 		Pending: *queue.New(),
 		TaskDb:  make(map[string][]*task.Task),
 		EventDb: make(map[string][]*task.TaskEvent),
-		Workers: []string{},
+		Workers: []string{w.Name},
 	}
 
 	fmt.Printf("manager: %v\n", m)
@@ -66,4 +65,47 @@ func main() {
 	}
 
 	fmt.Printf("node: %v\n", n)
+
+	fmt.Printf("create a test container\n")
+	dockerTask, createResult := createContainer()
+
+	time.Sleep(time.Second * 5)
+
+	fmt.Printf("stopping container %s\n", createResult.ContainerID)
+	_ = stopContainer(dockerTask, createResult.ContainerID)
+}
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=cube",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+	fmt.Printf(
+		"Container %s is running with config %v\n", result.ContainerID, c)
+	return &d, &result
+}
+
+func stopContainer(d *task.Docker, id string) *task.DockerResult {
+	result := d.Stop(id)
+	if result.Error != nil{
+		fmt.Printf("%v\n", result.Error)
+		return nil
+	}
+
+	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerID)
+
+	return &result
 }
