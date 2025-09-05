60fa124e63072de2e184b249a852677c3258bc79
Adit0507 <adi05072002@gmail.com>
2025-09-05 12:39:30 +0530

    state transition logic for task lifecycle

---
+++ task/state_machine.go
@@ -0,0 +1,31 @@
+package task
+
+const (
+	Pending   State = iota
+	Scheduled       //means system has figured out where to run the task
+	Running
+	Completed
+	Failed
+)
+
+var stateTransitionMap = map[State][]State{
+	Pending:   []State{Scheduled},
+	Scheduled: []State{Scheduled, Running, Failed},
+	Running:   []State{Running, Completed, Failed},
+	Completed: []State{},
+	Failed:    []State{},
+}
+
+func Contains(states []State, state State) bool {
+	for _, s := range states {
+		if s == state {
+			return true
+		}
+	}
+
+	return false
+}
+
+func ValidStateTransition(src State, dst State) bool {
+	return  Contains(stateTransitionMap[src], dst)
+}
