420f2dea272abf14588aa944297b1835ae738143
Adit0507 <adi05072002@gmail.com>
2025-09-05 15:11:11 +0530

    task lifecycle management with state validation

--- worker/worker.go
+++ worker/worker.go
@@ -1,7 +1,10 @@
 package worker
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/Adit0507/cube/task"
 	"github.com/golang-collections/collections/queue"
@@ -19,13 +22,77 @@ func (w *Worker) CollectionStats() {
 	fmt.Println("I will collect stats")
 }
 
-func (w *Worker) RunTask() {
-	fmt.Println("I will start or stop a task")
+func (w *Worker) RunTask() task.DockerResult {
+	t := w.Queue.Dequeue() 
+	if t == nil {
+		log.Println("No tasks in queuee")
+		return task.DockerResult{Error: nil}
+	}
+
+	taskQueued := t.(task.Task) //convertin task to proper type
+
+	taskPersisted := w.Db[taskQueued.ID] //atemptin to retrieve same task from db
+	if taskPersisted == nil {
+		taskPersisted = &taskQueued
+		w.Db[taskQueued.ID] = &taskQueued
+	}
+
+	var ans task.DockerResult
+	if task.ValidStateTransition(taskPersisted.State, taskQueued.State) {
+		switch taskQueued.State {
+		case task.Scheduled:
+			ans = w.StartTask(taskQueued)	//if theres a valid state transition and task from queue has state of scheduled, calling the StartTask method
+
+		case task.Completed:
+			ans = w.StopTask(taskQueued)
+
+		default:
+			ans.Error = errors.New("We shouldnt get here")
+		}
+	} else {
+		err := fmt.Errorf("Invalid transition from %v to %v", taskPersisted.State, taskQueued.State)
+
+		ans.Error = err //if there no valid transition, settin the error field of ans
+	}
+
+	return ans
 }
 
-func (w *Worker) StartTask() {
-	fmt.Println("I will start a task")
+func (w *Worker) AddTask(t task.Task) {
+	w.Queue.Enqueue(t)
 }
-func (w *Worker) StopTask() {
-	fmt.Println("I will stop a task")
+
+func (w *Worker) StartTask(t task.Task) task.DockerResult {
+	t.StartTime = time.Now().UTC()
+	config := task.NewConfig(&t)
+	d := task.NewDocker(config)
+	result := d.Run()
+	if result.Error != nil {
+		log.Printf("Error running task %v: %v\n", t.ID, result.Error)
+		t.State = task.Failed
+		w.Db[t.ID] = &t
+		return result
+	}
+
+	t.ContainerID = result.ContainerID
+	t.State = task.Running
+	w.Db[t.ID] = &t
+
+	return result
+}
+func (w *Worker) StopTask(t task.Task) task.DockerResult {
+	config := task.NewConfig(&t)
+	d := task.NewDocker(config)
+
+	result := d.Stop(t.ContainerID)
+	if result.Error != nil {
+		log.Printf("Error stopping container %v: %v\n", t.ContainerID, result.Error)
+	}
+
+	t.FinishTime = time.Now().UTC()
+	t.State = task.Completed
+	w.Db[t.ID] = &t
+	log.Printf("Stopped and removed container %v for task %v\n", t.ContainerID, t.ID)
+
+	return result
 }
